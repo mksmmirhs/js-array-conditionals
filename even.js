0b811bbb5108ac57be63f0955639676ba7dad0d5
Merajul Islam <meraj.exe@gmail.com>
2023-01-29 20:24:11 +0530

    problem

---
+++ even.js
@@ -0,0 +1,7 @@
+var evenNum = 1;
+while (evenNum <= 10) {
+  if (evenNum % 2 === 0) {
+    console.log(evenNum);
+  }
+  evenNum++;
+}
--- problem.js
+++ problem.js
@@ -7,3 +7,33 @@ console.log(fruits);
 fruits.pop();
 fruits.push("Watermelon");
 console.log(fruits);
+
+// problem 2
+var moneyGiven = 1000;
+var totalCost = 700;
+var returnAmount = moneyGiven - totalCost;
+console.log(returnAmount);
+
+// problem 3
+num1 = 113;
+num2 = 54;
+num3 = 114;
+if (num1 > num2) {
+  if (num1 > num3) {
+    console.log(num1);
+  } else {
+    console.log(num3);
+  }
+} else {
+  if (num2 > num3) {
+    console.log(num2);
+  } else {
+    console.log(num3);
+  }
+}
+
+let loopControl = 0;
+while (loopControl <= 100) {
+  console.log("আসকে আমার মন ভালো নেই");
+  loopControl++;
+}
